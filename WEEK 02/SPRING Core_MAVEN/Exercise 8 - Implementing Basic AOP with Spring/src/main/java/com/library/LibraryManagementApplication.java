9ac07d0c959c39d27a56264d5e980328c5910a4f
Harshitha-Loganathan <139323441+Harshitha-Loganathan@users.noreply.github.com>
2024-08-09 18:33:45 +0530

    first commit

---
+++ WEEK 02/SPRING Core_MAVEN/Exercise 8 - Implementing Basic AOP with Spring/src/main/java/com/library/LibraryManagementApplication.java
@@ -0,0 +1,15 @@
+package com.library;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.library.service.BookService;
+
+public class LibraryManagementApplication {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService bookService = context.getBean(BookService.class);
+
+        // Test adding a book to trigger aspect logging
+        bookService.addBook("Design Patterns");
+    }
+}
